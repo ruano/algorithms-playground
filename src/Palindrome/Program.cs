94eb9c2c13c7ed906c8bfe5cd115ac984379b6c4
Ruano Martinez Schulze <ruano.martinez@gmail.com>
2025-09-01 20:12:45 -0300

    feat: checking palindrome words

--- src/Palindrome/Program.cs
+++ src/Palindrome/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+Console.WriteLine("Write the word or phrase:");
+
+var input = Console.ReadLine();
+
+input = input!.Trim().ToLower();
+
+StringBuilder sb = new();
+
+for (int i = input.Length - 1; i < input.Length && i >= 0; i--)
+{
+    sb.Append(input[i]);
+}
+
+bool isPalindrome = sb.ToString().Equals(input, StringComparison.InvariantCultureIgnoreCase);
+
+Console.WriteLine($"Is palindrome? {isPalindrome}");
+
+Console.ReadKey();
