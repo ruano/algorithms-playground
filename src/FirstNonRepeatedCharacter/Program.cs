abae7814b7db7150e5ec5a6041fa8f11f66685be
Ruano Martinez Schulze <ruano.martinez@gmail.com>
2025-09-03 20:24:06 -0300

    feat: FirstNonRepeatedCharacter algorithm

---
+++ src/FirstNonRepeatedCharacter/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Write the word:");
+
+var input = Console.ReadLine() ?? string.Empty;
+
+input = input.Trim().ToLower();
+
+Dictionary<char, int> counts = [];
+
+foreach (var c in input)
+{
+    if (counts.ContainsKey(c))
+        counts[c]++;
+    else
+        counts[c] = 1;
+}
+
+char? firstNonRepeated = input.FirstOrDefault(c => counts[c] == 1);
+
+var message = firstNonRepeated == default(char)
+    ? "All characters are repeated"
+    : $"First non-repeated character: {firstNonRepeated}";
+
+Console.WriteLine(message);
+Console.ReadKey();
