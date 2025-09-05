72cef9222dc1ea4ec11ca8b3dd4115f135864c95
Ruano Martinez Schulze <ruano.martinez@gmail.com>
2025-09-05 19:38:51 -0300

    feat: FirstNonRepeatedCharacterMultiplesQueries

---
+++ FirstNonRepeatedCharacterMultiplesQueries/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Write base string:");
+string baseString = Console.ReadLine() ?? string.Empty;
+
+List<string> queries = new() { "abacabad", "xyz", "aabb" };
+
+Dictionary<string, char?> finalResult = new();
+
+foreach (string query in queries)
+{
+    char? firstNonRepeated = null;
+    int startIndex = baseString.IndexOf(query, StringComparison.OrdinalIgnoreCase);
+
+    if (startIndex != -1)
+    {
+        Dictionary<char, int> counts = new();
+
+        for (int i = startIndex; i < startIndex + query.Length; i++)
+        {
+            char c = baseString[i];
+            counts[c] = counts.GetValueOrDefault(c) + 1;
+        }
+
+        for (int i = startIndex; i < startIndex + query.Length; i++)
+        {
+            if (counts[baseString[i]] == 1)
+            {
+                firstNonRepeated = baseString[i];
+                break;
+            }
+        }
+    }
+
+    finalResult.Add(query, firstNonRepeated);
+}
+
+// Output
+foreach (var kv in finalResult)
+{
+    Console.WriteLine($"{kv.Key}: {(kv.Value.HasValue ? kv.Value.ToString() : "null")}");
+}
+
+
+
+Console.ReadKey();
