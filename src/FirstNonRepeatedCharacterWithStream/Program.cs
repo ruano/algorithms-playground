133192e6b18f5943f50f90bea02f87d75b0f33ca
Ruano Martinez Schulze <ruano.martinez@gmail.com>
2025-10-15 20:59:52 -0300

    feat: Add FirstNonRepeatedCharacterWithStream project
    Introduced a new console application to find the first non-repeated character in a character stream. Updated the solution file to include the new project.

---
+++ src/FirstNonRepeatedCharacterWithStream/Program.cs
@@ -0,0 +1,41 @@
+﻿const string stream = "abcbad";
+
+Queue<char> queue = [];
+
+foreach (var item in stream)
+{
+    var nonRepeatedChar = FirstNonRepeatedCharacterFromQueue(queue, item);
+
+    foreach (var itemQueue in queue)
+    {
+        Console.Write(itemQueue + " ");        
+    }
+
+    var resultChar = nonRepeatedChar == '\0' ? "none" : nonRepeatedChar.ToString();
+
+    Console.WriteLine("-> " + resultChar);
+}
+
+Console.ReadKey();
+
+static char FirstNonRepeatedCharacterFromQueue(Queue<char> queue, char c)
+{
+    queue.Enqueue(c);
+
+    Dictionary<char, int> counts = [];
+
+    foreach (var item in queue)
+    {
+        counts[item] = counts.GetValueOrDefault(item) + 1;
+    }
+
+    foreach (var item in counts)
+    {
+        if (item.Value == 1)
+        {
+            return item.Key;
+        }
+    }
+
+    return '\0';
+}
